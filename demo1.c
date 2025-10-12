1d7ce2b6298679f014d6a5414d8dc2f4b0947b6f
zagralmariyam <zagralmariyam1@gmail.com>
2025-10-13 01:55:55 +0530

    Add files via upload

---
+++ demo1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main () {
+   /*Name: Mariyam Zagral
+     age: 18
+     Class: FE (COMPUTER ENGG)
+     Batch: D
+     UIN: 251P084*/
+
+     printf("Name: Mariyam Zagral\n");
+     printf("Age:18\n");
+     printf("Class: FE(Computer Engg)\n");
+     printf("UIN: 251P084\n");
+     printf("Batch: D\n");
+
+    return 0;
+}
