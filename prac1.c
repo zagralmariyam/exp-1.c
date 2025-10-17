8eb6b3b741e4f6100074bddc4db6c5da9dafaf08
zagralmariyam <zagralmariyam1@gmail.com>
2025-10-17 13:46:55 +0530

    Add files via upload

---
+++ prac1.c
@@ -0,0 +1,65 @@
+/* Name: Mariyam Zagral
+     UIN:251P084
+     Class: Comp / D */
+
+#include<stdio.h>
+
+int main() {
+
+int choice;
+ float num1, num2, result;
+
+start:
+
+printf("\n-- Simple Calculator ---\n");
+printf("1. Addition\n");
+printf("2. Subtraction\n");
+ printf("3. Multiplication\n");
+printf("4. Division\n");
+printf("5. Exit\n");
+
+printf("Enter your choice: ");
+scanf("%d", &choice);
+
+
+if (choice == 5) {
+printf("Exiting...\n");
+return 0;
+
+}
+printf("Enter two numbers: ");
+scanf("%f %f", &num1, &num2);
+
+switch (choice) {
+
+case 1:
+result =num1 + num2;
+printf("Result = %.2f\n", result);
+break;
+
+case 2:
+result = num1- num2;
+printf("Result = %.2f\n", result);
+break;
+
+case 3:
+result=num1*num2;
+printf("Result = %.2f\n", result);
+break;
+
+case 4:
+if (num2 == 0)
+printf("Cannot divide by zerol\n");
+else {
+result = num1 / num2;
+printf("Result = %.2f\n", result);
+}
+break;
+
+default:
+printf("Invalid choice!\n");
+
+ }
+goto start;
+
+ }
