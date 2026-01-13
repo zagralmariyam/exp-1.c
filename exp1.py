5202833349c87f3f1d64ba49733ff31c5e0b7afa
zagralmariyam <zagralmariyam1@gmail.com>
2026-01-14 00:59:23 +0530

    Add files via upload

---
+++ exp1.py
@@ -0,0 +1,11 @@
+print("Mariyam")
+print("251P084")
+
+name = input ("What is your name?: ")
+hobby = input ("What is your favorite hobby?: ")
+food = input("What is your favorite food?: ")
+
+print("\nHello,"+name+"!")
+print("It's great that you enjoy "+ hobby +".")
+print("I hope you get to eat some " + food + " today!")
+print("Have an amazing day " + name + "!")
