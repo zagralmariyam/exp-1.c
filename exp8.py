968a41bbd7e1b02fa2364bd207aca3a2e83540ef
zagralmariyam <zagralmariyam1@gmail.com>
2026-02-03 00:00:00 +0530

    Add files via upload

---
+++ exp8.py
@@ -0,0 +1,14 @@
+
+#Mariyam Zagral
+#251P084
+
+print("Mariyam Zagral")
+print("251P084")
+
+
+rows = 5
+
+for i in range(1, rows + 1):
+    for j in range(i):
+        print("*", end="")
+    print()
