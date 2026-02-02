26c803c0663791b6c66622c2be913ff08e5126e9
zagralmariyam <zagralmariyam1@gmail.com>
2026-02-03 00:00:38 +0530

    Add files via upload

---
+++ exp9.py
@@ -0,0 +1,12 @@
+#Mariyam Zagral
+#251P084
+
+print("Mariyam Zagral")
+print("251P084")
+
+import math 
+def factorial(n): 
+ return(math.factorial(n)) 
+num = int(input("Enter the number:"))
+print("Factorial of", num, "is",
+ factorial(num))
